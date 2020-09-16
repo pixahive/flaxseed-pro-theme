8a2f1786d3bfa353f65d05ce4feda6afcfe0e0ec
Elucinator <shaikhkamranirfan1@gmail.com>
2020-09-16 12:34:17 +0530

    Fixed get_theme_mod error in register_styles.php

--- modules/admin_modules/register_styles.php
+++ modules/admin_modules/register_styles.php
@@ -14,21 +14,32 @@ function flaxseedpro_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	// Get title font set by user
-	$title_font = get_theme_mod( 'flaxseedpro_title_font' );
-	if ( array_key_exists( $title_font, $google_fonts_arr ) ) {
-		wp_enqueue_style( 'flaxseed-pro-title-font', '//fonts.googleapis.com/css?family=' . urlencode( $google_fonts_arr[ $title_font ] ) . ':300,400,600,700,400italic,700italic' );
-	} else {
-		wp_enqueue_style( 'flaxseed-pro-title-font', '//fonts.googleapis.com/css?family=Work+Sans:300,400,600,700,400italic,700italic' );
-	}
-	// Get body font set by user
-	$body_font = get_theme_mod( 'flaxseedpro_body_font' );
-	if ( array_key_exists( $body_font, $google_fonts_arr ) ) {
-		wp_enqueue_style( 'flaxseed-pro-body-font', '//fonts.googleapis.com/css?family=' . urlencode( $google_fonts_arr[ $body_font ] ) . ':300,400,600,700,400italic,700italic' );
-	} else {
-		wp_enqueue_style( 'flaxseed-pro-body-font', '//fonts.googleapis.com/css?family=Libre+Baskerville:300,400,600,700,400italic,700italic' );
-	}
-
+        // Check if theme_mod exists.
+        if(get_theme_mod( 'flaxseedpro_title_font' )){
+            // If exist get title font set by user.
+            $title_font = get_theme_mod( 'flaxseedpro_title_font' );
+            if ( array_key_exists( $title_font, $google_fonts_arr ) ) {
+                wp_enqueue_style( 'flaxseed-pro-title-font', '//fonts.googleapis.com/css?family=' . urlencode( $google_fonts_arr[ $title_font ] ) . ':300,400,600,700,400italic,700italic' );
+            } else {
+                wp_enqueue_style( 'flaxseed-pro-title-font', '//fonts.googleapis.com/css?family=Work+Sans:300,400,600,700,400italic,700italic' );
+            }
+        }else{
+            // Use theme default font.
+            wp_enqueue_style( 'flaxseed-pro-title-font', '//fonts.googleapis.com/css?family=Work+Sans:300,400,600,700,400italic,700italic' );
+        }
+        // Check if theme_mod exists.
+        if(get_theme_mod( 'flaxseedpro_body_font' )){
+            // If exist get body font set by user.
+            $body_font = get_theme_mod( 'flaxseedpro_body_font' );
+            if ( array_key_exists( $body_font, $google_fonts_arr ) ) {
+                wp_enqueue_style( 'flaxseed-pro-body-font', '//fonts.googleapis.com/css?family=' . urlencode( $google_fonts_arr[ $body_font ] ) . ':300,400,600,700,400italic,700italic' );
+            } else {
+                 // Use theme default font.
+                wp_enqueue_style( 'flaxseed-pro-body-font', '//fonts.googleapis.com/css?family=Libre+Baskerville:300,400,600,700,400italic,700italic' );
+            }  
+        } else {
+            wp_enqueue_style( 'flaxseed-pro-body-font', '//fonts.googleapis.com/css?family=Libre+Baskerville:300,400,600,700,400italic,700italic' );
+        }
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
 
 	wp_enqueue_script( 'owlcarousel-js', get_template_directory_uri() . '/assets/owlcarousel/dist/owl.carousel.min.js' );
